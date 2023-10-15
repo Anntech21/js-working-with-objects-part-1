0b8d298be9533ba38d076b8f56c9a46909a9eb70
Ann Tech <anntech21@gmail.com>
2023-10-14 20:55:08 -0700

    completed

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ let user2 = {
 };
 
 // Log the variable's value in the console.
+console.log(user2);
 //{name: 'Anchal Jain', age: 39, language: 'Hindi', programmer:'false'}
 
 // Create a variable called "dino1" that points to a new object. It should have the keys "name", "species", and "diet". The values should be "T-Rex", "Tyrannosaurus Rex", and "carnivore", respectively.
@@ -34,6 +35,7 @@ let dino1 = {
 };
 
 // Log the variable's value in the console.
+console.log(dino1);
 //{name: 'T-Rex', species: 'Tyrannosaurus', diet: 'carnivore'}
 
 // Create an object to hold information on your favorite recipe. It should have properties for:
@@ -47,9 +49,10 @@ let dino1 = {
 let favoriteRecipe = {
   Title: "Pancake",
   servings: 3,
-  Ingredents: "Maple", "milk", "eggs", "batter",
+  Ingredients: ["Maple", "milk", "eggs", "batter"],
 };
 
+console.log(favoriteRecipe);
 /********************** Exercise 2 - Accessing Values **********************/
 
 // Create a variable that holds the value of the "title" key in the "recipe" object you created above.
@@ -74,12 +77,14 @@ let user3 = {
 };
 
 user3.language;
+
 // Log the value of the "language" key in the "user3" object to the console.
 
 // Put the value of the "language" key in the "user3" object into a new variable. Log the value of the first language in that array to the console using an index.
 
 let user3Language = user3.language;
 user3.language[0];
+
 // WITHOUT creating a new variable or accessing the one you just created, log the value of the first language in the "languages" array to the console. You'll have to use BOTH dot notation to access the "languages" array, and square bracket notation to access the first element in that array, all in the same line.
 user3.language[0];
 /********************** Exercise 3 - Adding Values **********************/
@@ -90,11 +95,13 @@ let dino2 = {};
 
 // Add a key-value pair to the above empty "dino2" object. The key should be "name", and the value should be "Raptor".
 dino2.name = "Ann";
+
 // Add a key-value pair to the "dino2" object. The key should be "species", and the value should be "Velociraptor".
 dino2["species"] = "Anchal";
 
 // Add a key-value pair to the "dino2" object. The key should be "diet", and the value should be "carnivore".
 dino2.diet = "Carnivore";
+
 // Log the "dino2" object to the console.
 
 let dino3 = {
@@ -108,10 +115,11 @@ let dino3 = {
 
 // Now create a dino4 object with the name and species keys. The values should be "Triceratops" and "Triceratops Horridus", respectively.
 let dino4 = {name: "Triceratop",
-species: "Triceratops Horridus",};
-// Add a key-value pair to the "dino4" object. The key should be "diet", and the value should be "herbivore".
+species: "Triceratops Horridus"};
 
+// Add a key-value pair to the "dino4" object. The key should be "diet", and the value should be "herbivore".
 dino4.diet = "Herbivore";
+
 // Log the "dino4" object to the console.
 console.log(dino4);
 /********************** Exercise 4 - Changing Values **********************/
@@ -125,12 +133,19 @@ let dino5 = {
 // ALL OF THE STEPS BELOW SHOULD BE DONE WITHOUT CHANGING THE CODE ABOVE.
 
 // Change the value of the "diet" key in the "dino5" object to "omnivore".
+dino5.diet = "omnivore";
 
 // Log the "dino5" object to the console.
+console.log(dino5);
 
 // Change the value of the "name" key in the "dino5" object to "Sarah".
 
+dino5["name"] = "Sarah";
+
 // Log the "dino5" object to the console.
+
+console.log(dino5);
+
 
 let prettyGoodBook = {
   title: "The Hobbit",
@@ -145,4 +160,6 @@ prettyGoodBook.price = 12.99;
 
 // Change the value of the "rating" key in the "prettyGoodBook" object to 5. (It's more than just pretty good!)
 prettyGoodBook.rating = 5;
+
 // Log the "prettyGoodBook" object to the console.
+console.log(prettyGoodBook);
